b26ad0b575b583efc332f8f81eac04878d3bfbde
ParasKatwal <katwal.paras@gmail.com>
2019-08-27 12:53:00 +0545

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>       
+<script src="assets_front/js/bootstrap.min.js"></script>       
+<script src="assets_front/js/custom.js"></script>  
+</body>
+</html>  
---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo Title ?></title>
+    <link rel="stylesheet" href="assets_front/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets_front/css/custom.css">
+    <link rel="stylesheet" href="assets_front/css/responsive.css" media="screen and (max-width: 1200px)">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+</head>
+<body> 
